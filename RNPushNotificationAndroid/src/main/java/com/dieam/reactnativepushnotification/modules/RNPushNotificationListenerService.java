02124b3d59d8d5f718cb4e3273e8d55c56f0aa52
ep-infosec <ep-infosec@users.noreply.github.com>
2022-12-09 20:46:29 +0200

    init

---
+++ RNPushNotificationAndroid/src/main/java/com/dieam/reactnativepushnotification/modules/RNPushNotificationListenerService.java
@@ -0,0 +1,47 @@
+package com.dieam.reactnativepushnotification.modules;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.app.ActivityManager;
+import android.app.ActivityManager.RunningAppProcessInfo;
+
+import java.util.List;
+
+import com.google.android.gms.gcm.GcmListenerService;
+
+public class RNPushNotificationListenerService extends GcmListenerService {
+
+    @Override
+    public void onMessageReceived(String from, Bundle bundle) {
+        sendNotification(bundle);
+    }
+
+    private void sendNotification(Bundle bundle) {
+
+        Boolean isRunning = isApplicationRunning();
+        
+        Intent intent = new Intent("RNPushNotificationReceiveNotification");
+        bundle.putBoolean("foreground", isRunning);
+        intent.putExtra("notification", bundle);
+        sendBroadcast(intent);
+
+        if (!isRunning) {
+            new RNPushNotificationHelper(getApplication(), this).sendNotification(bundle);
+        }
+    }
+
+    private boolean isApplicationRunning() {
+        ActivityManager activityManager = (ActivityManager) this.getSystemService(ACTIVITY_SERVICE);
+        List<RunningAppProcessInfo> processInfos = activityManager.getRunningAppProcesses();
+        for (ActivityManager.RunningAppProcessInfo processInfo : processInfos) {
+            if (processInfo.processName.equals(getApplication().getPackageName())) {
+                if (processInfo.importance == ActivityManager.RunningAppProcessInfo.IMPORTANCE_FOREGROUND) {
+                    for (String d: processInfo.pkgList) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+}
